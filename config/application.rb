535f964cff95c288fe636625e94670387f50c412
Louis Wheeler <louiswheeleriv@gmail.com>
2024-05-24 03:56:23 -0400

    Configure app as API only

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module ModelExplorer
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
     config.x.encryption_key = ENV.fetch('ENCRYPTION_KEY')
   end
 end
