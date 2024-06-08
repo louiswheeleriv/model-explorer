8b85fee8928ce91f02f8a8d26d58427f1970285f
Louis Wheeler <louiswheeleriv@gmail.com>
2024-06-07 20:38:50 -0400

    Adj cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins [Rails.configuration.x.client_domain]
-    # origins { |source, env| true }
-    origins [/localhost/, /model-explorer\.com/]
+    origins { |source, env| true }
+    # origins [/localhost/, /model-explorer\.com/]
     resource '*', headers: :any, methods: :any, credentials: true
   end
 end
