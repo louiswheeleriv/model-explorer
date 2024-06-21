32adac4c425c6bb64c6421c02703c7d91ad3d52a
Louis Wheeler <louiswheeleriv@gmail.com>
2024-06-21 10:30:33 -0400

    Fix color bar corner rounding

--- app/javascript/common/StatusColorBar.tsx
+++ app/javascript/common/StatusColorBar.tsx
@@ -20,6 +20,28 @@ type Props = {
   size: 'large' | 'small';
 }
 
+function statusSegmentRadiusClass(segmentIndex: number, numSegments: number, roundingMode: string): string {
+  const isFirstSegment = segmentIndex === 0;
+  const isLastSegment = segmentIndex === numSegments - 1;
+  if (isFirstSegment && isLastSegment) {
+    switch (roundingMode) {
+      case 'all': return 'rounded-md';
+      case 'bottom': return 'rounded-b-md';
+    }    
+  } else if (isFirstSegment) {
+    switch (roundingMode) {
+      case 'all': return 'rounded-l-md';
+      case 'bottom': return 'rounded-bl-md';
+    }  
+  } else if (isLastSegment) {
+    switch (roundingMode) {
+      case 'all': return 'rounded-r-md';
+      case 'bottom': return 'rounded-br-md';
+    }  
+  }
+  return '';
+}
+
 const StatusColorBar = (props: Props) => {
   let numByStatus = props.numByStatus;
   let numByStatusZeroCase = { unassembled: 1, assembled: 0, in_progress: 0, finished: 0 };
@@ -37,20 +59,7 @@ const StatusColorBar = (props: Props) => {
     let pct = (numInStatus / totalNumModels) * 100;
     let roundedPercent = roundToNearest(pct, 5);
 
-    let borderRadiusClass = '';
-    if (i === 0) {
-      if (props.rounding === 'all') {
-        borderRadiusClass = 'rounded-l-md';
-      } else if (props.rounding === 'bottom') {
-        borderRadiusClass = 'rounded-bl-md';
-      }
-    } else if (i === statusesWithModels.length - 1) {
-      if (props.rounding === 'all') {
-        borderRadiusClass = 'rounded-r-md';
-      } else if (props.rounding === 'bottom') {
-        borderRadiusClass = 'rounded-br-md';
-      }
-    }
+    const borderRadiusClass = statusSegmentRadiusClass(i, statusesWithModels.length, props.rounding);
 
     let height = 'h-[10px]';
     if (props.size === 'large') height = 'h-[60px]';
