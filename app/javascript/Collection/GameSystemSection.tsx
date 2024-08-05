8c28278fdceef0c8ffd03bd22d499faa3d1ea5af
Louis Wheeler <louiswheeleriv@gmail.com>
2024-08-05 18:22:38 -0400

    Consolidate collapsible section component

--- app/javascript/Collection/GameSystemSection.tsx
+++ app/javascript/Collection/GameSystemSection.tsx
@@ -1,9 +1,7 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React from "react";
 import { GameSystem, Faction, UserFaction } from "../types/models";
 import FactionProgressBar from "./FactionProgressBar";
-import $ from 'jquery';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-902717d512/icons';
+import CollapsibleListSection from "../common/CollapsibleListSection";
 
 type Props = {
   gameSystem: GameSystem;
@@ -17,39 +15,21 @@ type Props = {
 };
 
 const GameSystemSection = (props: Props) => {
-  const [isExpanded, setIsExpanded] = useState(props.startExpanded);
-
-  useEffect(() => {
-    const marginTop = isExpanded ? '0' : '-100%';
-    const angle = isExpanded ? 0 : -90;
-    $('#'+componentId+' .faction-sections').css({ 'margin-top': marginTop });
-    $('#'+componentId+' .collapse-icon').css({ 'transform': 'rotate('+angle+'deg)' })
-  }, [isExpanded]);
-
-  const componentId = 'game-system-section-' + props.gameSystem.id;
-
   return (
-    <div className={props.className} id={componentId}>
-      <div className='mb-5 text-xl w-fit cursor-pointer' onClick={() => setIsExpanded(!isExpanded)}>
-        <span className='mr-3'>
-          <FontAwesomeIcon icon={byPrefixAndName.fas['chevron-down']} className='collapse-icon transition-transform duration-300' />
-        </span>
-        {props.gameSystem.name}
-      </div>
-      <div className='faction-sections-container overflow-hidden'>
-        <div className='faction-sections transition-margin-top duration-1000 delay-0'>
-          {props.userFactionSections.map((userFactionSection) => (
-            <Fragment key={userFactionSection.userFaction.id}>
-              <FactionProgressBar
-                faction={userFactionSection.faction}
-                userFaction={userFactionSection.userFaction}
-                numByStatus={userFactionSection.factionNumByStatus}
-                className='mb-5'/>
-            </Fragment>
-          ))}
-        </div>
-      </div>
-    </div>
+    <CollapsibleListSection
+      id={'game-system-section-' + props.gameSystem.id}
+      headerText={props.gameSystem.name}
+      numItems={props.userFactionSections.length}>
+
+      {props.userFactionSections.map((userFactionSection) => (
+        <FactionProgressBar
+          key={userFactionSection.userFaction.id}
+          faction={userFactionSection.faction}
+          userFaction={userFactionSection.userFaction}
+          numByStatus={userFactionSection.factionNumByStatus}
+          className='mb-5'/>
+      ))}
+    </CollapsibleListSection>
   );
 };
 
--- app/javascript/Explore/UserModelSection.tsx
+++ app/javascript/Explore/UserModelSection.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Model, User, UserModel } from "../types/models";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { byPrefixAndName } from '@awesome.me/kit-902717d512/icons';
 import UserModelSectionItem from "./UserModelSectionItem";
-import $ from 'jquery';
+import CollapsibleListSection from "../common/CollapsibleListSection";
 
 type Props = {
   user: User;
@@ -13,50 +11,25 @@ type Props = {
 };
 
 const UserModelSection = (props: Props) => {
-  const [isExpanded, setIsExpanded] = useState(true);
-
-  function redirectToUserModelPage(userModel: UserModel) {
-    window.location.assign('/user-models/'+userModel.id);
-  }
-
-  useEffect(() => {
-    const itemHeight = 120;
-    const maxHeight = isExpanded ? (itemHeight * props.userModels.length) + 'px' : 0;
-    const angle = isExpanded ? 90 : 0;
-    $('#'+componentId+' .models').css({
-      'max-height': maxHeight
-    });
-    $('#'+componentId+' .group-collapse-icon').css({ 'transform': 'rotate('+angle+'deg)' });
-  }, [isExpanded]);
-
-  const componentId = 'user-model-section-'+props.user.id;
-
   return (
-    <div className={props.className} id={componentId}>
-      <div className='group-name my-5 text-xl cursor-pointer' onClick={() => setIsExpanded(!isExpanded)}>
-        <FontAwesomeIcon
-          icon={byPrefixAndName.fas['chevron-right']}
-          className='group-collapse-icon transition-transform duration-300 mr-3' />
-        {props.user.display_name || props.user.username}
-      </div>
-      <div className='overflow-hidden'>
-        <div className='models transition-[max-height] ease-in-out duration-500'>
-          {props.userModels.length === 0 &&
-            <div className='text-xl text-center my-5'>
-              No Models
-            </div>
-          }
+    <CollapsibleListSection
+      id={'user-model-section-'+props.user.id}
+      headerText={props.user.display_name || props.user.username}
+      numItems={props.userModels.length}>
 
-          {props.userModels.map((userModel: UserModel) => (
-            <UserModelSectionItem
-              key={userModel.id}
-              model={props.model}
-              userModel={userModel}
-              className={'mb-5'} />
-          ))}
+      {props.userModels.length === 0 &&
+        <div className='text-xl text-center my-5'>
+          No Models
         </div>
-      </div>
-    </div>
+      }
+      {props.userModels.map((userModel: UserModel) => (
+        <UserModelSectionItem
+          key={userModel.id}
+          model={props.model}
+          userModel={userModel}
+          className={'mb-5'} />
+      ))}
+    </CollapsibleListSection>
   );
 };
 
