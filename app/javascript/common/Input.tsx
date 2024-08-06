b36a1431903155ff2a472fa264a1294b0e9ab967
Louis Wheeler <louiswheeleriv@gmail.com>
2024-08-06 18:31:15 -0400

    Change input font size to 16px

--- app/javascript/common/Input.tsx
+++ app/javascript/common/Input.tsx
@@ -15,27 +15,25 @@ type Props = {
 
 const Input = (props: PropsWithChildren<Props>) => {
   return (
-    <>
-      <input
-        id={props.id}
-        type={props.type || 'text'}
-        className={
-          (props.className ? props.className+' ' : '')+
-          'block border text-sm rounded-lg w-full p-2.5 '+
-          'bg-gray-600 border-gray-500 placeholder-gray-400 '+
-          'disabled:text-[#aaaaaa] '+
-          'focus:ring-primary-500 focus:border-primary-500'
-        }
-        placeholder={props.placeholder}
-        defaultValue={props.defaultValue}
-        value={props.value}
-        onChange={props.onChange}
-        disabled={props.disabled}
-        min={props.min}
-        max={props.max}>
-          {props.children}
-      </input>
-    </>
+    <input
+      id={props.id}
+      type={props.type || 'text'}
+      className={
+        (props.className ? props.className+' ' : '')+
+        'block border text-[16px] rounded-lg w-full p-2.5 '+
+        'bg-gray-600 border-gray-500 placeholder-gray-400 '+
+        'disabled:text-[#aaaaaa] '+
+        'focus:ring-primary-500 focus:border-primary-500'
+      }
+      placeholder={props.placeholder}
+      defaultValue={props.defaultValue}
+      value={props.value}
+      onChange={props.onChange}
+      disabled={props.disabled}
+      min={props.min}
+      max={props.max}>
+        {props.children}
+    </input>
   );
 };
 
