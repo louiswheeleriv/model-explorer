8afbdaea5f5b5f352eddd049f44b85ef290909d7
Louis Wheeler <louiswheeleriv@gmail.com>
2025-01-03 21:49:09 -0500

    Adj name to MiniPainter in UI

--- app/javascript/Welcome.tsx
+++ app/javascript/Welcome.tsx
@@ -14,14 +14,14 @@ const Welcome = (props: Props) => {
           <FontAwesomeIcon icon={byPrefixAndName.fas['dice']} size='xl' />
         </div>
         <div className='flex-1 text-2xl text-center my-auto max-w-[300px]'>
-          Welcome to Model Explorer!
+          Welcome to MiniPainter!
         </div>
         <div className='flex-none my-auto ml-3'>
           <FontAwesomeIcon icon={byPrefixAndName.fas['paintbrush-fine']} size='xl' />
         </div>
       </div>
       <p className='text-center mb-5'>
-        Model Explorer is a tool for you to manage and display your collection of miniatures, and explore those of your friends.
+        MiniPainter is a tool for you to manage and display your collection of miniatures, and explore those of your friends.
       </p>
       <p className='text-center'>
         Many features are still in development. Check back often for updates!
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins [Rails.configuration.x.client_domain]
     origins { |source, env| true }
-    # origins [/localhost/, /model-explorer\.com/]
+    # origins [/localhost/, /mini-painter\.com/]
     resource '*', headers: :any, methods: :any, credentials: true, if: ->(x) { true }
   end
 end
