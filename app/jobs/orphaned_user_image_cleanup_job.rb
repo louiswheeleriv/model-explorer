944a41b64110976bb04ad68bc4938d5d22f033e0
Louis Wheeler <louiswheeleriv@gmail.com>
2025-01-07 18:10:50 -0500

    Fix orphaned image job deleting profile pics

--- app/jobs/orphaned_user_image_cleanup_job.rb
+++ app/jobs/orphaned_user_image_cleanup_job.rb
@@ -2,9 +2,13 @@ class OrphanedUserImageCleanupJob
   def perform
     Rails.logger.info("Cleaning up orphaned UserImages")
     count = 0
+
+    model_and_faction_image_ids = UserImageAssociation.distinct.pluck(:user_image_id)
+    profile_picture_ids = User.distinct.pluck(:profile_picture_id).compact
+
     UserImage
       .where
-      .not(id: UserImageAssociation.distinct.pluck(:user_image_id))
+      .not(id: (model_and_faction_image_ids + profile_picture_ids).uniq)
       .find_each do |user_image|
         count += 1
         user_image.destroy!
